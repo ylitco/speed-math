c995aa0df43e12c986e876bf95c15bc05bdedf0c
Egor Litviakov <ylitvyakov@yandex.ru>
2022-03-28 10:58:13 +0300

    feat(views): add Overview gui layout

--- src/views/Overview/Overview.tsx
+++ src/views/Overview/Overview.tsx
@@ -1,10 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from 'src/components/Button/Button';
+import { Close as CloseIcon } from 'src/icons/Close/Close';
+
+import { BUTTON_TYPE } from 'src/components/Button/types';
+
 import styles from '../views.module.scss';
+import localStyles from './Overview.module.scss';
 
 export const Overview: FC = () => {
+  const navigate = useNavigate();
+  const handleClose = useCallback(_handleClose, [navigate]);
+
   return (
     <main className={styles.view}>
-      <h1>Overview View</h1>
+      <video className={localStyles.overview} />
+      <Button className={localStyles.close} type={BUTTON_TYPE.LIMPID} onClick={handleClose}>
+        <CloseIcon />
+      </Button>
     </main>
   );
+
+  function _handleClose() {
+    navigate(-1);
+  }
 };
