0fb8e4343cab335910be428214ef69b3a039c98e
Egor Litviakov <ylitvyakov@yandex.ru>
2022-03-28 14:16:13 +0300

    feat(views): add About gui layout
    Refactor Info icon

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -1,10 +1,21 @@
 import React, { FC } from 'react';
+import cn from 'classnames';
+
+import { TEXT } from 'src/views/About/constants';
+
 import styles from '../views.module.scss';
+import localStyles from './About.module.scss';
+
+/**
+ * @todo
+ *  1. Use Markdown in description
+ *  2. Use internationalization
+ */
 
 export const About: FC = () => {
   return (
-    <main className={styles.view}>
-      <h1>About View</h1>
+    <main className={cn(styles.view, localStyles.description)}>
+      <div className={localStyles.content}>{TEXT}</div>
     </main>
   );
 };
