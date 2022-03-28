7517da0da4345b4163536867c9ac19ddc4e2f7d3
Egor Litviakov <ylitvyakov@yandex.ru>
2022-03-28 14:31:04 +0300

    feat(views): add Start gui layout

--- src/views/Start/Start.tsx
+++ src/views/Start/Start.tsx
@@ -1,14 +1,35 @@
-import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
+import React, { FC, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Wheel } from 'src/components/Wheel/Wheel';
+
+import { VIEW } from 'src/views/constants';
+
+import { IEventMetaObject } from 'src/types';
+import { TWheelValue } from 'src/components/Wheel/types';
+
 import styles from '../views.module.scss';
+import localStyles from './Start.module.scss';
+
+const OPTIONS = {
+  [VIEW.GAME_SETTINGS]: 'Take exercise',
+  [VIEW.ABOUT]: 'About',
+  [VIEW.OVERVIEW]: 'Overview',
+};
+const DEFAULT_OPTION = VIEW.OVERVIEW;
 
 export const Start: FC = () => {
+  const navigate = useNavigate();
+  const handleSelect = useCallback(_handleSelect, [navigate]);
+
   return (
     <main className={styles.view}>
-      <h1>Start View</h1>
-      <Link to="/game-settings">Take exercise</Link>
-      <Link to="/about">About</Link>
-      <Link to="/overview">Overview</Link>
+      <Wheel className={localStyles.menu} options={OPTIONS} value={DEFAULT_OPTION} onSelect={handleSelect} />
+      <h2 className={localStyles.hint}>Scroll to select</h2>
     </main>
   );
+
+  function _handleSelect(e: IEventMetaObject<TWheelValue>) {
+    navigate(`/${e.value}`);
+  }
 };
