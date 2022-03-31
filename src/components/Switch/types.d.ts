80abbfc354e5363a1f3585c2b326f2246f189a26
Egor Litviakov <ylitvyakov@yandex.ru>
2022-03-31 23:52:43 +0300

    fix(switch): make label prop optional

--- src/components/Switch/types.d.ts
+++ src/components/Switch/types.d.ts
@@ -4,8 +4,8 @@ type TSwitchValue = string;
 type TSwitchTitle = string | ReactElement;
 
 export interface ISwitchProps extends HTMLAttributes<ReactElement> {
-  label: string;
   options: Record<TSwitchValue, TSwitchTitle>;
   value: TSwitchValue;
   onChange: (TSwitchValue) => void;
+  label?: string;
 }
