1d783513e4c3dc8fef0f695605a229fa1f3ae299
Egor Litviakov <ylitvyakov@yandex.ru>
2022-03-31 23:54:12 +0300

    feat(views): add GameSettings gui layout

--- src/views/GameSettings/GameSettings.tsx
+++ src/views/GameSettings/GameSettings.tsx
@@ -1,13 +1,98 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState } from 'react';
+import cn from 'classnames';
 import styles from '../views.module.scss';
-import { Link } from 'react-router-dom';
+import localStyles from './GameSettings.module.scss';
+import { Button } from 'src/components/Button/Button';
+import { Switch } from 'src/components/Switch/Switch';
+import { InfoIcon } from 'src/icons/Info/Info';
+import { TimerIcon } from 'src/icons/Timer/Timer';
+import { QuantityIcon } from 'src/icons/Quantity/Quantity';
+import { InfinityIcon } from 'src/icons/Infinity/Infinity';
+import { IEventMetaObject } from 'src/types';
+import { PlayIcon } from 'src/icons/Play/Play';
+
+const MODES = [3, 4, 5, 6, 7, 8, 9, 11, 12, 'N'];
 
 export const GameSettings: FC = () => {
+  const [mode, setMode] = useState('time');
+  const [practice, setPractice] = useState<Record<string, string>>({});
+  const handleModeChange = useCallback(_handleModeChange, []);
+  const handleDifficultyChange = useCallback(_handleDifficultyChange, []);
+  const handlePlayClick = useCallback(_handlePlayClick, [mode, practice]);
+
   return (
-    <main className={styles.view}>
-      <h1>Game Settings View</h1>
-      <Link to="/game">Game</Link>
-      <Link to="/explanation">Explanation</Link>
+    <main className={cn(styles.view, localStyles.view)}>
+      <b className={localStyles.easy}>easy</b>
+      <b className={localStyles.medium}>medium</b>
+      <b className={localStyles.hard}>hard</b>
+      {MODES.map((_mode: string | number) => {
+        const mode = _mode.toString();
+        return (
+          <div className={localStyles.row} key={mode}>
+            <div className={localStyles.number}>
+              ×{mode}
+            </div>
+            <div className={localStyles.easy}>
+              <input
+                type='radio'
+                name={mode}
+                value="easy"
+                checked={practice[mode] === 'easy'}
+                onChange={(e) => handleDifficultyChange(e.target.value, mode)}
+              />
+            </div>
+            <div className={localStyles.medium}>
+              <input
+                type='radio'
+                name={mode}
+                value="medium"
+                checked={practice[mode] === 'medium'}
+                onChange={(e) => handleDifficultyChange(e.target.value, mode)}
+              />
+            </div>
+            <div className={localStyles.hard}>
+              <input
+                type='radio'
+                name={mode}
+                value="hard"
+                checked={practice[mode] === 'hard'}
+                onChange={(e) => handleDifficultyChange(e.target.value, mode)}
+              />
+            </div>
+            <InfoIcon />
+          </div>
+        );
+      })}
+      <Switch
+        className={localStyles.modes}
+        options={{
+          time: <TimerIcon />,
+          quantity: <QuantityIcon />,
+          unlimited: <InfinityIcon />,
+        }}
+        value={mode}
+        onChange={handleModeChange}
+      />
+      <Button className={localStyles.play} onClick={handlePlayClick}>
+        <PlayIcon />
+      </Button>
     </main>
   );
+
+  function _handleModeChange(e: IEventMetaObject<string>) {
+    setMode(e.value);
+  }
+
+  function _handleDifficultyChange(value: string, mode: string) {
+    setPractice((prevPractice) => {
+      return { ...prevPractice, [mode]: value };
+    });
+  }
+
+  function _handlePlayClick() {
+    console.log(
+      mode,
+      practice,
+    );
+  }
 };
