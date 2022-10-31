a985878cbe24c0d05bed88e8b6e08d749c34c7b2
Yegor Litvyakov <Egor.Litvyakov@leroymerlin.ru>
2022-10-31 06:47:38 +0300

    feat(input): implement markup

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import styles from './Input.module.scss';
+import { StateContext } from '../../state';
+import { INPUT_MODE } from 'src/state/constants';
 
 interface IInputProps {
   answer: number | null;
@@ -6,8 +9,25 @@ interface IInputProps {
   secondFactor: number;
 }
 
-export const Input: FC<IInputProps> = (props) => {
+export const Input: FC<IInputProps> = ({ firstFactor, secondFactor, answer }) => {
+  const result = (firstFactor * secondFactor).toString().split('');
+  const _answer = answer !== null ? answer.toString().split('') : [];
+  const { settings: { global: { inputMode } } } = useContext(StateContext);
+  const view = inputMode === INPUT_MODE.LTR ?
+    Array(result.length - _answer.length ).fill('').concat(_answer) :
+    Array(result.length - _answer.length ).fill('').concat(_answer.reverse());
+
   return (
-    <input value={!props.answer ? '' : props.answer} onChange={() => {}} />
+    <div className={styles.input}>
+      {view.map((value, index) => {
+        return (
+          <div key={index + value} className={styles.inputCell}>
+            <p className={styles.outerShadow}>{value}</p>
+            <p className={styles.stroke}>{value}</p>
+            <p className={styles.innerGradient}>{value}</p>
+          </div>
+        )
+      })}
+    </div>
   );
 };
