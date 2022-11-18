9c88d411f0d01f0492cba053d26c53f684d0ec43
Yegor Litvyakov <Egor.Litvyakov@leroymerlin.ru>
2022-11-18 21:30:37 +0300

    feat(animation): add `useSibling`

--- src/views/pages/Explanation/CalcPresenter/animations/useSibling.tsx
+++ src/views/pages/Explanation/CalcPresenter/animations/useSibling.tsx
@@ -1,8 +1,18 @@
+import { gsap } from 'gsap';
 import CalcPresenter from '../CalcPresenter';
+import { STEP_RESULT } from './const';
 
 export function useSibling(this: CalcPresenter) {
-    const msg = `Забываем про ${this.inAttention} и дальше работаем с соседом ${this.sibling}`;
-    console.log(msg);
+    console.log(`Забываем про ${this.inAttention} и дальше работаем с соседом ${this.sibling}`);
     this.inAttention = this.sibling;
-    this.stepInstructionsArea.innerHTML = msg;
+
+    const [stepResultSlot] = this.stepResult;
+    const [siblingTwinSlot, siblingTwinDigit] = this.siblingTwin;
+
+    const tl = gsap.timeline();
+
+    tl.to(stepResultSlot, { width: 0, opacity: 0, padding: 0, onComplete: () => stepResultSlot.remove() })
+      .to(siblingTwinDigit, { opacity: 1, paddingLeft: 3, onComplete: () => siblingTwinSlot.setAttribute('id', STEP_RESULT) });
+
+    this.tl.add(tl);
 }
