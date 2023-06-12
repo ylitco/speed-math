7c2705bce4783ffe7045af2a02ef3bbe100c705f
Yegor Litvyakov <Egor.Litvyakov@leroymerlin.ru>
2023-06-12 07:08:08 +0300

    feat(wheel) handle one finger touch scroll

--- src/components/Wheel/Wheel.tsx
+++ src/components/Wheel/Wheel.tsx
@@ -1,4 +1,5 @@
 import React, { FC, MouseEvent, useCallback, useState } from 'react';
+import type { Touch, TouchEvent, TouchEventHandler } from 'react';
 import cn from 'classnames';
 import { IWheelProps } from 'src/components/Wheel/types';
 import { createEventMetaObject } from 'src/utils';
@@ -6,10 +7,16 @@ import styles from './Wheel.module.scss';
 import _ from 'lodash';
 import { Protector } from './components/Protector/Protector';
 
+interface RelativeTouch extends Pick<Touch, 'identifier' | 'pageX' | 'pageY'> {
+  initialYpoint: number;
+  deltaY: number;
+};
+
 export const Wheel: FC<IWheelProps<any>> = (props) => {
   const { onSelect, options: _options, value, selectType = 'auto' } = props;
   const handleClick = useCallback(_handleClick, [onSelect, value]);
   const [activeValueIndex, setActiveValueIndex] = useState(Object.keys(_options).indexOf(value));
+  const [ongoingTouches, setOngoingTouches] = useState<Array<RelativeTouch>>([]);
   const [touchIndex, setTouchIndex] = useState(0);
   const lastValueIndex = Object.keys(_options).length - 1;
   const options = Object.entries(_options);
@@ -26,13 +33,7 @@ export const Wheel: FC<IWheelProps<any>> = (props) => {
 
     const delta = Math.floor(e.deltaY / 100);
 
-    let newActiveIndex = activeValueIndex - delta;
-
-    if (newActiveIndex < 0) {
-      newActiveIndex = lastValueIndex;
-    } else if (newActiveIndex > lastValueIndex) {
-      newActiveIndex = 0;
-    }
+    const newActiveIndex = calcNewActiveIndex(activeValueIndex, delta);
 
     if (selectType === 'auto') {
       onSelect(createEventMetaObject(options[newActiveIndex][0]));
@@ -40,37 +41,86 @@ export const Wheel: FC<IWheelProps<any>> = (props) => {
 
     setActiveValueIndex(newActiveIndex);
   }, 100), [activeValueIndex, value]);
-  const handleTouchMove = useCallback(_.throttle((e: any) => {
-    const newTouchIndex = Math.ceil(e.targetTouches[0].clientY / 100);
-    let delta = 0;
+  const calcNewActiveIndex = useCallback((index: number, delta: number) => {
+    let newIndex = index - delta;
 
-    if (newTouchIndex > touchIndex) {
-      delta -= 1;
-    } else {
-      delta += 1;
+    if (newIndex < 0) {
+      newIndex = lastValueIndex;
+    } else if (newIndex > lastValueIndex) {
+      newIndex = 0;
     }
 
-    setTouchIndex(newTouchIndex);
-
-    let newActiveIndex = activeValueIndex - delta;
-
-    if (newActiveIndex < 0) {
-      newActiveIndex = lastValueIndex;
-    } else if (newActiveIndex > lastValueIndex) {
-      newActiveIndex = 0;
+    return newIndex;
+  }, [lastValueIndex]);
+  const ongoingTouchIndexById = useCallback((idToFind) => {
+    for (let i = 0; i < ongoingTouches.length; i++) {
+      if (ongoingTouches[i].identifier === idToFind) {
+        return i;
+      }
     }
+    return -1;
+  }, [ongoingTouches]);
+  const handleTouchStart = useCallback((e: TouchEvent) => {
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      const newTouch = copyTouch(e.changedTouches[i]) as RelativeTouch;
 
-    if (selectType === 'auto') {
-      onSelect(createEventMetaObject(options[newActiveIndex][0]));
+      newTouch.initialYpoint = newTouch.pageY;
+      newTouch.deltaY = 0;
+
+      setOngoingTouches([...ongoingTouches, newTouch]);
     }
+  }, [ongoingTouches]);
+  const handleTouchEnd = useCallback<TouchEventHandler<HTMLDivElement>>((e) => {
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      let idx = ongoingTouchIndexById(e.changedTouches[i].identifier);
 
-    setActiveValueIndex(newActiveIndex);
-  }, 300), [activeValueIndex]);
+      if (idx >= 0) {
+        const touches = [...ongoingTouches];
+
+        touches.splice(idx, 1);
+
+        setOngoingTouches(touches);
+      }
+    }
+  }, [ongoingTouches]);
+  const handleTouchCancel = handleTouchEnd;
+  const handleTouchMove = useCallback<TouchEventHandler<HTMLDivElement>>((e) => {
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      const idx = ongoingTouchIndexById(e.changedTouches[i].identifier);
+
+      if (idx >= 0) {
+        const touches = [...ongoingTouches];
+        const newTouch = copyTouch(e.changedTouches[i]) as RelativeTouch;
+
+        newTouch.initialYpoint = touches[idx].initialYpoint;
+        newTouch.deltaY = Math.round((newTouch.initialYpoint - newTouch.pageY) / 75);
+
+        if (touches[idx].deltaY !== newTouch.deltaY) {
+          const delta = touches[idx].deltaY - newTouch.deltaY;
+
+          const newActiveIndex = calcNewActiveIndex(activeValueIndex, delta);
+
+          if (selectType === 'auto') {
+            onSelect(createEventMetaObject(options[newActiveIndex][0]));
+          }
+
+          setActiveValueIndex(newActiveIndex);
+        }
+
+        touches.splice(idx, 1, newTouch);
+
+        setOngoingTouches(touches);
+      }
+    }
+  }, [activeValueIndex, lastValueIndex, onSelect, options, selectType, touchIndex]);
 
   return (
     <div
       className={cn(props.className, styles.wheel, styles[props.size])}
       onWheel={handleWheel}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+      onTouchCancel={handleTouchCancel}
       onTouchMove={handleTouchMove}
     >
       <div className={cn(styles.border, styles.left)} />
@@ -101,3 +151,7 @@ export const Wheel: FC<IWheelProps<any>> = (props) => {
     onSelect(createEventMetaObject(e.currentTarget.dataset['value']))
   }
 };
+
+function copyTouch({ identifier, pageX, pageY }: Touch) {
+  return { identifier, pageX, pageY };
+}
