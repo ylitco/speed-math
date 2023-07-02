8c646b34f36fde5f32c31b748bb6c46ba7947975
Yegor Litvyakov <Egor.Litvyakov@leroymerlin.ru>
2023-07-02 07:21:50 +0300

    Refactor Keyboard component code style

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,37 +1,54 @@
-import React, { FC, useCallback, useEffect } from 'react';
-import { Button } from 'src/components/Button/Button';
-import { createEventMetaObject } from 'src/utils';
-import { IKeyboardProps } from './types';
-import styles from './Keyboard.module.scss';
-import { Check } from './components/Check/Check';
-import Delete from './components/Delete/Delete';
-import { Calculator } from './components/Calculator/Calculator';
-import cn from 'classnames';
-import { CHECKING_MODE } from '../../state/constants';
-import { getCheckMode } from 'src/state/Workout';
-import { useSelector } from 'react-redux';
+import React, { FC, useCallback, useEffect } from "react";
+import { Button } from "src/components/Button/Button";
+import { createEventMetaObject } from "src/utils";
+import { IKeyboardProps } from "./types";
+import styles from "./Keyboard.module.scss";
+import { Check } from "./components/Check/Check";
+import Delete from "./components/Delete/Delete";
+import { Calculator } from "./components/Calculator/Calculator";
+import cn from "classnames";
+import { CHECKING_MODE } from "../../state/constants";
+import { getCheckMode } from "src/state/Workout";
+import { useSelector } from "react-redux";
 
-const Keyboard: FC<IKeyboardProps> = ({ onClick, answer, isReady, onReady, result, ...props }) => {
+const Keyboard: FC<IKeyboardProps> = ({
+  onClick,
+  answer,
+  isReady,
+  onReady,
+  result,
+  ...props
+}) => {
   const checkMode = useSelector(getCheckMode);
-  const handleNumberClick = useCallback((e) => {
-    const number = +e.currentTarget.dataset.name;
-    onClick(createEventMetaObject(answer !== null ? `${answer}${number}` : number));
-  }, [answer, onClick]);
+  const handleNumberClick = useCallback(
+    (e) => {
+      const number = +e.currentTarget.dataset.name;
+      onClick(
+        createEventMetaObject(answer !== null ? `${answer}${number}` : number)
+      );
+    },
+    [answer, onClick]
+  );
   const handleBackspaceClick = useCallback(() => {
-    const result = answer !== null && ('' + answer).length > 1 ? (answer.toString().slice(0, -1)) : null
+    const result =
+      answer !== null && ("" + answer).length > 1
+        ? answer.toString().slice(0, -1)
+        : null;
     onClick(createEventMetaObject(result));
   }, [answer, onClick]);
 
   useEffect(() => {
-    if (answer && answer.toString().length === result.toString().length && checkMode === CHECKING_MODE.AUTO) {
+    if (
+      answer &&
+      answer.toString().length === result.toString().length &&
+      checkMode === CHECKING_MODE.AUTO
+    ) {
       props.onCheck();
     }
   }, [answer]);
 
   if (!isReady) {
-    return (
-      <Calculator onClick={onReady} className={styles.calculator} />
-    );
+    return <Calculator onClick={onReady} className={styles.calculator} />;
   }
 
   return (
@@ -45,9 +62,19 @@ const Keyboard: FC<IKeyboardProps> = ({ onClick, answer, isReady, onReady, resul
           </Button>
         );
       })}
-      {checkMode === CHECKING_MODE.HAND ? <Button onClick={props.onCheck}><Check /></Button> : <div />}
-      <Button name="0" onClick={handleNumberClick}><span>0</span></Button>
-      <Button onClick={handleBackspaceClick}><Delete /></Button>
+      {checkMode === CHECKING_MODE.HAND ? (
+        <Button onClick={props.onCheck}>
+          <Check />
+        </Button>
+      ) : (
+        <div />
+      )}
+      <Button name="0" onClick={handleNumberClick}>
+        <span>0</span>
+      </Button>
+      <Button onClick={handleBackspaceClick}>
+        <Delete />
+      </Button>
     </div>
   );
 };
