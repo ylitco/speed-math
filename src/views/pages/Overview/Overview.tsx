961a0997489a1c38dd14b95249aa6403d5f06c73
Yegor Litvyakov <Egor.Litvyakov@leroymerlin.ru>
2023-07-07 13:43:29 +0300

    Add overview video

--- src/views/pages/Overview/Overview.tsx
+++ src/views/pages/Overview/Overview.tsx
@@ -11,10 +11,24 @@ export const Overview: FC = () => {
   const handleClose = useCallback(() => {
     navigate(-1);
   }, [navigate]);
+  const handleVideoFinish = useCallback(() => {
+    navigate(-1);
+  }, [navigate]);
 
   return (
     <Content>
-      <video className={styles.overview} />
+      <video
+        className={styles.overview}
+        src="/about.mp4"
+        autoPlay={true}
+        onEnded={handleVideoFinish}
+      >
+        <source src="/about.mp4" type="video/mp4"></source>
+        Sorry, your browser doesn't support videos. Write to the developer of
+        this app, and in any way, very soon this overview will be uploaded on
+        video hostings web sites
+      </video>
+
       <Button
         className={styles.close}
         type={BUTTON_TYPE.LIMPID}
