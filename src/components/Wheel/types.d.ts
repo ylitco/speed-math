acd2c0272e1832ac9d05700f8c654239bdde4d4c
Egor Litviakov <ylitvyakov@yandex.ru>
2024-11-12 09:46:40 +0300

    refactor: specify Wheel props type

--- src/components/Wheel/types.d.ts
+++ src/components/Wheel/types.d.ts
@@ -1,11 +1,13 @@
-import { HTMLAttributes, ReactElement } from 'react';
-import { IEventMetaObject } from 'src/types';
+import { HTMLAttributes, ReactElement } from "react";
+import { IEventMetaObject } from "src/types";
+
+type WheelTitle = string | ReactElement | FC<HTMLAttributes<HTMLOrSVGElement>>;
 
 export interface IWheelProps<T = any> extends HTMLAttributes<ReactElement> {
-  options: Record<T, string>;
+  options: Record<T, WheelTitle>;
   value: T;
   onSelect: (e: IEventMetaObject<T>) => void;
   scale?: number;
-  size: 'L' | 'M' | 'S';
-  selectType?: 'auto' | 'click';
+  size: "L" | "M" | "S";
+  selectType?: "auto" | "click";
 }
