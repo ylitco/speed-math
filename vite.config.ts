134af22c295905d7c40283a2308b410f8e0e968b
Egor Litviakov <ylitvyakov@yandex.ru>
2024-12-26 14:27:39 +0300

    feat: make app progressive

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,54 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg,mp4,json"] },
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "mask-icon.png",
+      ],
+      manifest: {
+        name: "Speed Math: Trachtenberg system",
+        short_name: "Speed Math",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "mask-icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#eaedfb",
+        background_color: "#eaedfb",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "/src"),
