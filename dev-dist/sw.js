7eeeb7f2d7b91b5aa114f8ac7fd8f6ab03858f26
Egor Litviakov <ylitvyakov@yandex.ru>
2026-01-28 10:24:25 +0300

    chore: update `@iconify/react`
    Think about replacing it with the lucide icon toolkit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9vldqs4rvn8"
+    "revision": "0.l7p3nld1n7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
