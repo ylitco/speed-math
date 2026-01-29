35095b1b4c65d2fa40ca369d959e47532103cc3f
Egor Litviakov <ylitvyakov@yandex.ru>
2026-01-29 08:20:49 +0300

    chore: update `vite` plugins dependencies
    - `@vite-pwa/assets-generator`
    - `vite-plugin-pwa`

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gtm5apmmltg"
+    "revision": "0.63u4d4s5hhg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
