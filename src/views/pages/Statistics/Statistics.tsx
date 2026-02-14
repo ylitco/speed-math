c8b37ac19115c338ccb9d64914484aad65adcc04
Egor Litviakov <ylitvyakov@yandex.ru>
2026-02-14 08:36:54 +0300

    style(statistics-page): fix formatting rules

--- src/views/pages/Statistics/Statistics.tsx
+++ src/views/pages/Statistics/Statistics.tsx
@@ -1,19 +1,19 @@
-import { FC, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { Header } from "~/components/Header/Header";
-import { BackButton } from "~/views/components/BackButton";
-import Button, { BUTTON_TYPE } from "~/components/Button";
-import { Content } from "~/components/Content/Content";
-import { getUrl } from "~/utils";
-import { VIEW } from "~/views/constants";
-import { useStartWorkoutCallback } from "~/hooks/useStartWorkoutEffect";
-import { Statistics as StatisticsIcon } from "./components/Statistics/Statistics";
-import styles from "./Statistics.module.scss";
-import { Repeat } from "./icons/Repeat";
-import { Burger } from "./icons/Burger";
-import { Tooltip } from "react-tooltip";
-import { useSelector } from "react-redux";
+import { FC, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { Header } from '~/components/Header/Header';
+import { BackButton } from '~/views/components/BackButton';
+import Button, { BUTTON_TYPE } from '~/components/Button';
+import { Content } from '~/components/Content/Content';
+import { getUrl } from '~/utils';
+import { VIEW } from '~/views/constants';
+import { useStartWorkoutCallback } from '~/hooks/useStartWorkoutEffect';
+import { Statistics as StatisticsIcon } from './components/Statistics/Statistics';
+import styles from './Statistics.module.scss';
+import { Repeat } from './icons/Repeat';
+import { Burger } from './icons/Burger';
+import { Tooltip } from 'react-tooltip';
+import { useSelector } from 'react-redux';
 import {
   finishSet,
   getCountingRate,
@@ -22,7 +22,7 @@ import {
   getSetStat,
   stopTimer,
   useAppDispatch,
-} from "~/state/Workout";
+} from '~/state/Workout';
 
 export const Statistics: FC = () => {
   const navigate = useNavigate();
@@ -41,15 +41,15 @@ export const Statistics: FC = () => {
 
   return (
     <>
-      <Header renderMinorAction={BackButton}>{t("statistics.title")}</Header>
+      <Header renderMinorAction={BackButton}>{t('statistics.title')}</Header>
       <Content className={styles.view}>
         <h2 className={styles.resultValue}>
           <span
             data-tooltip-id="counting-rate-unit"
-            data-tooltip-content={t("statistics.countingRateUnit")}
+            data-tooltip-content={t('statistics.countingRateUnit')}
             data-tooltip-place="bottom"
           >
-            {`${countingRate} ${t("statistics.cru")}`}
+            {`${countingRate} ${t('statistics.cru')}`}
           </span>
         </h2>
         <StatisticsIcon
