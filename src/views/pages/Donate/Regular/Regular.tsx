6daa01b2f66e26d2dffe51d958850543d878b84d
Egor Litviakov <ylitvyakov@yandex.ru>
2026-02-14 08:55:44 +0300

    style(donate-page): fix formatting rules

--- src/views/pages/Donate/Regular/Regular.tsx
+++ src/views/pages/Donate/Regular/Regular.tsx
@@ -1,17 +1,17 @@
-import { Header } from "~/components/Header/Header";
-import { BackButton } from "~/views/components/BackButton";
-import { SettingsButton } from "~/views/components/SettingsButton";
-import classes from "../Donate.module.scss";
-import { Wheel } from "~/components/Wheel/Wheel";
-import { useState } from "react";
-import { Icon } from "@iconify/react";
+import { Header } from '~/components/Header/Header';
+import { BackButton } from '~/views/components/BackButton';
+import { SettingsButton } from '~/views/components/SettingsButton';
+import classes from '../Donate.module.scss';
+import { Wheel } from '~/components/Wheel/Wheel';
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
 
 const SUPPORT_OPTION = {
-  "0.5K": <Icon width="2em" height="2em" icon="hugeicons:coffee-02" />,
-  "1K": <Icon width="2em" height="2em" icon="pepicons-pencil:electricity" />,
+  '0.5K': <Icon width="2em" height="2em" icon="hugeicons:coffee-02" />,
+  '1K': <Icon width="2em" height="2em" icon="pepicons-pencil:electricity" />,
 };
 
-const DEFAULT_SUPPORT_OPTION = "0.5K";
+const DEFAULT_SUPPORT_OPTION = '0.5K';
 
 const Regular = () => {
   const [support, setSupport] = useState(DEFAULT_SUPPORT_OPTION);
