937223db9e1188daf67ad3966b50c1166d41c202
Egor Litviakov <ylitvyakov@yandex.ru>
2026-02-14 11:12:46 +0300

    style(vite-config): fix formatting rules

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,55 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: 'autoUpdate',
       devOptions: { enabled: true },
-      workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg,json}"] },
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.png"],
+      workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'] },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
       manifest: {
-        name: "Speed Math: Trachtenberg system",
-        short_name: "Speed Math",
+        name: 'Speed Math: Trachtenberg system',
+        short_name: 'Speed Math',
         icons: [
           {
-            src: "pwa-64x64.png",
-            sizes: "64x64",
-            type: "image/png",
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
           },
           {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
           {
-            src: "mask-icon.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
+            src: 'mask-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
-        start_url: ".",
-        display: "standalone",
-        theme_color: "#eaedfb",
-        background_color: "#eaedfb",
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#eaedfb',
+        background_color: '#eaedfb',
       },
     }),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "/src"),
+      '~': path.resolve(__dirname, '/src'),
     },
   },
 });
